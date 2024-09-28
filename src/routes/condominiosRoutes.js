6f54ddd031986dc388442b56ce14499ab3f715e8
sprightlytree2 <luccasdaccosta@gmail.com>
2024-09-27 23:40:53 -0300

    rota teste

--- src/routes/condominiosRoutes.js
+++ src/routes/condominiosRoutes.js
@@ -3,6 +3,9 @@ import CondominioController from "../controllers/condominioController.js";
 
 const routes = express.Router();
 
+routes.get("/test", (req, res) => {
+    res.send("A rota de teste está funcionando!");
+});
 routes.get("/condominios/:id", CondominioController.listarCondominioPorId);
 routes.post("/condominios", CondominioController.cadastrarCondominio);
 routes.put("/condominios/:id", CondominioController.atualizarCondominio);
