86ccfecc7c1a76833ad5c08bc7a980d73cc43312
sprightlytree2 <luccasdaccosta@gmail.com>
2024-09-29 18:11:05 -0300

    Subindo correção de login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -7,15 +7,19 @@ class LoginController{
             const email = req.body.email;
             const senha = req.body.senha;
 
-            var resposta = await ProcuraMorador(email, senha, res);
+            var resposta = await ProcuraMorador(email, senha);
 
             if(resposta.codigo == 200){
-                return resposta.express_res;
+                return res.status(200).json({ id_usuario: resposta.usuario._id, tipo_usuario: resposta.tipo_usuario});
             }
 
-            resposta = await ProcurarCondominio(email, senha, res);
+            resposta = await ProcurarCondominio(email, senha);
 
-            return resposta.express_res;
+            if(resposta.codigo == 200){
+                return res.status(200).json({ id_usuario: resposta.usuario._id, tipo_usuario: resposta.tipo_usuario});
+            }
+
+            return res.status(400).json("Usuario e/ou senha invalidos");
 
         } catch (error) {
             res.status(500).json({ message: `${error.message} - Falha ao listar condominio por id`});
@@ -23,24 +27,24 @@ class LoginController{
     }
 }
 
-async function ProcuraMorador(email, senha, res) {
+async function ProcuraMorador(email, senha) {
     const moradorEncontrado = await moradorRepo.find({ $and: [{email: email}, {senha: senha}] });
 
     if(moradorEncontrado.length == 0){
-        return { codigo: 400, express_res: null }
+        return { codigo: 400 }
     }
 
-    return { codigo: 200, express_res: res.status(200).json( {id_usuario: moradorEncontrado[0]._id, tipo_usuario: "morador"} )};
+    return { codigo: 200, usuario: moradorEncontrado[0], tipo_usuario: "morador"};
 }
 
-async function ProcurarCondominio(email, senha, res) {
+async function ProcurarCondominio(email, senha) {
     const condominioEncontrado = await condominioRepo.find({ $and: [{email_adm: email}, {senha_adm: senha}] });
 
     if(condominioEncontrado.length == 0){
-        return { codigo: 400, express_res: res.status(400).json("Usuario e/ou senha inválidos") }
+        return { codigo: 400 }
     }
 
-    return { codigo: 200, express_res: res.status(200).json( {id_usuario: condominioEncontrado[0]._id, tipo_usuario: "condominio"} )};
+    return { codigo: 200, usuario: condominioEncontrado[0], tipo_usuario: "condominio"};
 }
 
 
